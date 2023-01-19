f1fd9ad8549a02436d1e851d246ca882eec9e7e8
rahulvellukkeel <rahul.vellukkeel@gmail.com>
2023-01-19 23:34:35 +0000

    Added err_ranges code from class. added curve fit and prediction

--- rahul_code.py
+++ rahul_code.py
@@ -8,6 +8,10 @@ Created on Thu Jan 19 16:23:31 2023
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.optimize as opt
+import seaborn as sns
+from sklearn import cluster
+import err_ranges as err
 
 def read_file(file_name):
         df = pd.read_excel(file_name)
@@ -29,9 +33,27 @@ def read_file(file_name):
         df_transposed["UK"] = pd.to_numeric(df_transposed["UK"])
         print(df_transposed)
         #print(df_transposed["INDIA"])
+        return df_changed, df_transposed
+
+def curve_fun(t, scale, growth):
+    """ Computes exponential function with scale and growth as free parameters
+    """
+    c = scale * np.exp(growth * (t-1970))
+    return c
+
+
+df_co2, df_co2t = read_file("E:/Herts/ADS1/Assignment 3/Rahul/co2_emission.xlsx")
+df_gdp, df_gdpt = read_file("E:/Herts/ADS1/Assignment 3/Rahul/gdp.xlsx")
+df_renew, df_renewt = read_file("E:/Herts/ADS1/Assignment 3/Rahul/Renewable.xlsx")
+
+
+param, cov = opt.curve_fit(curve_fun,df_co2t["year"],df_co2t["INDIA"],p0=[4e8, 0.1])
+sigma = np.sqrt(np.diag(cov))
+print(*param)
+low,up = err.err_ranges(df_co2t["year"],curve_fun,param,sigma)
 
 
 
-read_file("E:/Herts/ADS1/Assignment 3/Rahul/co2_emission.xlsx")
-read_file("E:/Herts/ADS1/Assignment 3/Rahul/gdp.xlsx")
-read_file("E:/Herts/ADS1/Assignment 3/Rahul/Renewable.xlsx")
+
+
+
